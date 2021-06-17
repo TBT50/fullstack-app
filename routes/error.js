2255986d5eff153aa2cfa7ceecb0ec253ec0beb9
Piotrek <piotrek380@gmail.com>
2021-06-17 22:21:50 +0200

    api - delete all files and directories

--- routes/error.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use((req, res) => {
-  res.status(404).render("error");
-});
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const { homePageView } = require("./../controllers/product");
-
-router.get("/", homePageView);
-module.exports = router;
